1fd6eaebadb98a1817cde5b7f3cb51a8b027ce09
dragusheen <nathan.tirolf@epitech.eu>
2024-11-03 14:33:10 +0100

    [+] upd trad + api url

--- services/fetch.ts
+++ services/fetch.ts
@@ -11,54 +11,57 @@ import { getToken } from "./token";
 
 
 /* ----- DATAS ----- */
-const API_URL = "http://192.168.1.28:8080/api";
+// const API_URL = "http://localhost:8080/api";
+const API_URL = "https://certain-catfish-splendid.ngrok-free.app/backend/api";
 
 
-/* ----- FUNCTIONS ----- */
+/* ----- PRIVATE FUNCTIONS ----- */
+const getHeaders = async () => {
+    const authToken = await getToken();
+    return {
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer " + authToken || "",
+        'Ngrok-Skip-Browser-Warning': "true",
+    };
+};
+
+
+/* ----- PUBLIC FUNCTIONS ----- */
 export async function fetchGet(url: string) {
     const completeUrl = `${API_URL}/${url}`;
-    const authToken = await getToken();
+    const headers = await getHeaders();
     return fetch(completeUrl, {
-        headers: {
-            Authorization: "Bearer " + authToken || "",
-        },
+        method: "GET",
+        headers: headers,
     });
 }
 
 export async function fetchPut(url: string, body: unknown) {
     const completeUrl = `${API_URL}/${url}`;
-    const authToken = await getToken();
+    const headers = await getHeaders();
     return fetch(completeUrl, {
         method: "PUT",
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: "Bearer " + authToken || "",
-        },
+        headers: headers,
         body: JSON.stringify(body),
     });
 }
 
 export async function fetchPost(url: string, body: unknown) {
     const completeUrl = `${API_URL}/${url}`;
-    const authToken = await getToken();
+    const headers = await getHeaders();
     return fetch(completeUrl, {
         method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: "Bearer " + authToken || "",
-        },
+        headers: headers,
         body: JSON.stringify(body),
     });
 }
 
 export async function fetchDelete(url: string) {
     const completeUrl = `${API_URL}/${url}`;
-    const authToken = await getToken();
+    const headers = await getHeaders();
     return fetch(completeUrl, {
         method: "DELETE",
-        headers: {
-            Authorization: "Bearer " + authToken || "",
-        },
+        headers: headers,
     });
 }
 
